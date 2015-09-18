fc99aa059ceb466f2564e4cc15227a6ae36561b1
Justin Podzimek <justin.podzimek@jpodz.local>
2015-09-17 19:33:37 -0600

    Changing file structure

---
+++ JPImageEditor/JPImageEditor.h
@@ -0,0 +1,19 @@
+//
+//  JPImageEditor.h
+//  JPImageEditor
+//
+//  Created by Justin Podzimek on 9/17/15.
+//  Copyright (c) 2015 Justin Podzimek. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JPImageEditor.
+FOUNDATION_EXPORT double JPImageEditorVersionNumber;
+
+//! Project version string for JPImageEditor.
+FOUNDATION_EXPORT const unsigned char JPImageEditorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JPImageEditor/PublicHeader.h>
+
+
